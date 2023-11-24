7ce4982e15a1f3eeaff3f8996bd7067fa161452a
Gerrit Grunwald <han.solo@mac.com>
2023-11-24 12:23:22 +0100

    cosmetics

--- restore_autocrac.sh
+++ restore_autocrac.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 START_TIME=$(($(date +'%s * 1000 + %-N / 1000000')))
-echo $START_TIME
-
 java -DSTART_TIME=$START_TIME -XX:CRaCRestoreFrom=./tmp_checkpoint
--- start_autocrac.sh
+++ start_autocrac.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 START_TIME=$(($(date +'%s * 1000 + %-N / 1000000')))
-echo $START_TIME
-
 java -DSTART_TIME=$START_TIME -Dspring.context.checkpoint=onRefresh -XX:CRaCCheckpointTo=./tmp_checkpoint -jar build/libs/nameservice-21.0.0.jar
