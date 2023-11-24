ba428aacafe14c693c3fb187dfa9aff6e42d7349
Gerrit Grunwald <han.solo@mac.com>
2023-11-24 12:21:36 +0100

    cosmetics

--- start_autocrac.sh
+++ start_autocrac.sh
@@ -3,4 +3,4 @@
 START_TIME=$(($(date +'%s * 1000 + %-N / 1000000')))
 echo $START_TIME
 
-java -DSTART_TIME=$START_TIME -Dspring.context.checkpoint=onRefresh -XX:CRaCCheckpointTo=./tmp_checkpoint -jar /opt/app/nameservice-21.0.0.jar
+java -DSTART_TIME=$START_TIME -Dspring.context.checkpoint=onRefresh -XX:CRaCCheckpointTo=./tmp_checkpoint -jar build/libs/nameservice-21.0.0.jar
